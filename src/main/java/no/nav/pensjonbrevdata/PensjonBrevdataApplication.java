3633d3b0f720f027620afe12104543bf1040d731
R154508 <sindre.rasmussen@nav.no>
2020-04-20 16:41:16 +0200

    Initial springboot setup.

---
+++ src/main/java/no/nav/pensjonbrevdata/PensjonBrevdataApplication.java
@@ -0,0 +1,13 @@
+package no.nav.pensjonbrevdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensjonBrevdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PensjonBrevdataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/nav/pensjonbrevdata/PensjonBrevdataApplicationTests.java
@@ -0,0 +1,13 @@
+package no.nav.pensjonbrevdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensjonBrevdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
