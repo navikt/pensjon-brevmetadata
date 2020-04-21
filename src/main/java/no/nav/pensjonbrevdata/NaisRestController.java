aea8465aae732c063e7f3c3b6069653eedba63e7
Hallaraker, Hans Magnus <Hans.Magnus.Hallaraker@nav.no>
2020-04-21 13:57:52 +0200

    PL-1411 Added NaisRestController

---
+++ src/main/java/no/nav/pensjonbrevdata/NaisRestController.java
@@ -0,0 +1,22 @@
+package no.nav.pensjonbrevdata;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/internal")
+public class NaisRestController {
+
+    @RequestMapping(path = "isAlive", method = RequestMethod.GET)
+    public ResponseEntity isAlive() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "isReady", method = RequestMethod.GET)
+    public ResponseEntity isReady() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
--- src/main/java/no/nav/pensjonbrevdata/PensjonBrevdataApplication.java
+++ src/main/java/no/nav/pensjonbrevdata/PensjonBrevdataApplication.java
@@ -9,5 +9,4 @@ public class PensjonBrevdataApplication {
     public static void main(String[] args) {
         SpringApplication.run(PensjonBrevdataApplication.class, args);
     }
-
 }
