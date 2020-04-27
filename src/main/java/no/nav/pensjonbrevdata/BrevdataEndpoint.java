2b9bed0637ef09ce245ec0f0fabd356ac07de749
Hallaraker, Hans Magnus <Hans.Magnus.Hallaraker@nav.no>
2020-04-27 15:03:34 +0200

    PL-1411  * Updated endpoints uri  * Added deploy.yaml script (not complete)  * Updated nais.yml removing unnecessary properties

--- src/main/java/no/nav/pensjonbrevdata/BrevdataEndpoint.java
+++ src/main/java/no/nav/pensjonbrevdata/BrevdataEndpoint.java
@@ -1,7 +1,7 @@
 package no.nav.pensjonbrevdata;
 
-import no.nav.pensjonbrevdata.model.Brev;
-import no.nav.pensjonbrevdata.model.SprakCode;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+import no.nav.pensjonbrevdata.model.Brev;
+import no.nav.pensjonbrevdata.model.SprakCode;
 
 @RestController
 @RequestMapping("api/brevdata")
 public class BrevdataEndpoint {
     private BrevdataProvider provider = new BrevdataProvider();
 
-    @GetMapping("/getSprakForBrevkode")
+    @GetMapping("/sprakForBrevkode")
     public List<SprakCode> getSprakForBrevkode(@RequestParam(value = "brevkode") String brevkode){
         try {
             return provider.getSprakForBrevkode(brevkode);
@@ -26,7 +27,7 @@ public class BrevdataEndpoint {
         }
     }
 
-    @GetMapping("/getBrevForBrevkode")
+    @GetMapping("/brevForBrevkode")
     public Brev getBrevForBrevkode(@RequestParam(value = "brevkode") String brevkode){
         try {
             return provider.getBrevForBrevkode(brevkode);
@@ -36,7 +37,7 @@ public class BrevdataEndpoint {
         }
     }
 
-    @GetMapping("getBrevdataForSaktype")
+    @GetMapping("brevdataForSaktype")
     public List<Brev> getBrevdatForSaktype(@RequestParam(value = "saktype") String saktype){
         try {
             return provider.getBrevdataForSaktype(saktype);
@@ -45,7 +46,7 @@ public class BrevdataEndpoint {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),e);
         }
     }
-    @GetMapping("getBrevkoderForSaktype")
+    @GetMapping("brevkoderForSaktype")
     public List<String> getBrevkoderForSaktype(@RequestParam(value = "saktype") String saktype){
         try {
             return provider.getBrevkoderForSaktype(saktype);
