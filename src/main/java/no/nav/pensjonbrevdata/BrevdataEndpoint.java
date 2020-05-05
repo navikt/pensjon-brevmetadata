4e934cd5316b92d46fb7d30bec10c629029cbac0
Hallaraker, Hans Magnus <Hans.Magnus.Hallaraker@nav.no>
2020-05-05 11:37:19 +0200

    PL-1411  * Updated endpoints to use PathVariable instead of RequestParams

--- src/main/java/no/nav/pensjonbrevdata/BrevdataEndpoint.java
+++ src/main/java/no/nav/pensjonbrevdata/BrevdataEndpoint.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -17,42 +17,43 @@ import no.nav.pensjonbrevdata.model.SprakCode;
 public class BrevdataEndpoint {
     private BrevdataProvider provider = new BrevdataProvider();
 
-    @GetMapping("/sprakForBrevkode")
-    public List<SprakCode> getSprakForBrevkode(@RequestParam(value = "brevkode") String brevkode){
+    @GetMapping("/sprakForBrevkode/{brevkode}")
+    public List<SprakCode> getSprakForBrevkode(@PathVariable(value = "brevkode") String brevkode) {
         try {
             return provider.getSprakForBrevkode(brevkode);
         } catch (Exception e) {
             //TODO: Logging her
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
         }
     }
 
-    @GetMapping("/brevForBrevkode")
-    public Brev getBrevForBrevkode(@RequestParam(value = "brevkode") String brevkode){
+    @GetMapping(value = "/brevForBrevkode/{brevkode}")
+    public Brev getBrevForBrevkode(@PathVariable(value = "brevkode") String brevkode) {
         try {
             return provider.getBrevForBrevkode(brevkode);
         } catch (Exception e) {
             //TODO: Logging her
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
         }
     }
 
-    @GetMapping("brevdataForSaktype")
-    public List<Brev> getBrevdatForSaktype(@RequestParam(value = "saktype") String saktype){
+    @GetMapping("/brevdataForSaktype/{saktype}")
+    public List<Brev> getBrevdatForSaktype(@PathVariable(value = "saktype") String saktype) {
         try {
             return provider.getBrevdataForSaktype(saktype);
         } catch (Exception e) {
             //TODO: Logging her
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
         }
     }
-    @GetMapping("brevkoderForSaktype")
-    public List<String> getBrevkoderForSaktype(@RequestParam(value = "saktype") String saktype){
+
+    @GetMapping("/brevkoderForSaktype/{saktype}")
+    public List<String> getBrevkoderForSaktype(@PathVariable(value = "saktype") String saktype) {
         try {
             return provider.getBrevkoderForSaktype(saktype);
         } catch (Exception e) {
             //TODO: Logging her
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(),e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
         }
     }
 }
