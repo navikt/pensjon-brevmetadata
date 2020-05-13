affd40ef0d3c432ed3492b1a8429dfe22ae7db20
R154508 <sindre.rasmussen@nav.no>
2020-05-13 12:35:34 +0200

    PL-1411: Added field to Doksysvedlegg

--- src/main/java/no/nav/pensjonbrevdata/model/DoksysVedlegg.java
+++ src/main/java/no/nav/pensjonbrevdata/model/DoksysVedlegg.java
@@ -6,13 +6,15 @@ import java.io.File;
 import java.io.IOException;
 
 public class DoksysVedlegg {
+    private String vedleggkode;
     private String dekode;
     private String dokumentmalId;
     private String dokumentmalFelleselementId;
     private String dokumentmal;
     private String dokumentmalFelleselement;
 
-    public DoksysVedlegg(String dekode, String dokumentmalId, String dokumentmalFelleselementId) {
+    public DoksysVedlegg(String vedleggkode, String dekode, String dokumentmalId, String dokumentmalFelleselementId) {
+        this.vedleggkode = vedleggkode;
         this.dekode = dekode;
         this.dokumentmalId = dokumentmalId;
         this.dokumentmalFelleselementId = dokumentmalFelleselementId;
@@ -64,5 +66,13 @@ public class DoksysVedlegg {
 
     public void setDokumentmalFelleselement(String dokumentmalFelleselement) {
         this.dokumentmalFelleselement = dokumentmalFelleselement;
+    }
+
+    public String getVedleggkode() {
+        return vedleggkode;
+    }
+
+    public void setVedleggkode(String vedleggkode) {
+        this.vedleggkode = vedleggkode;
     }
 }
