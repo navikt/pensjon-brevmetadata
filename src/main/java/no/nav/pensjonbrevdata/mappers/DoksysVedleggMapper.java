c36d2c23a1b78443653afa95d2b009efb90652d8
R154508 <sindre.rasmussen@nav.no>
2020-05-13 12:35:13 +0200

    PL-1411: Added brevdata and xsd-files from database

--- src/main/java/no/nav/pensjonbrevdata/mappers/DoksysVedleggMapper.java
+++ src/main/java/no/nav/pensjonbrevdata/mappers/DoksysVedleggMapper.java
@@ -13,20 +13,55 @@ public class DoksysVedleggMapper {
 
     public DoksysVedleggMapper() {
         vedleggMap = new HashMap<>();
-        vedleggMap.put("RETTIGH_SERIOUSLY_VEDLEGG_V4", () -> new DoksysVedlegg(
-                "Vedlegg om andre ting",
-                "V0001",
-                "00002"));
-        vedleggMap.put("RETTIGH_UNCOOL_VEDLEGG_V2", () ->
+        vedleggMap.put("RETTIGH_PLIKT_V1", () ->
                 new DoksysVedlegg(
-                        "Vedlegg om utbetaling",
-                        "V0002",
-                        "00002"));
-        vedleggMap.put("RETTIGH_COOL_VEDLEGG_V1", () ->
+                        "RETTIGH_PLIKT_V1",
+                        "VEDLEGG: Orientering om rettigheter og plikter. Versjon 1",
+                        "V00002",
+                        "00001"));
+        vedleggMap.put("AP_MND_UTB_V1", () ->
                 new DoksysVedlegg(
-                        "Vedlegg om utbetaling",
-                        "V0003",
-                        "00002"));
+                        "AP_MND_UTB_V1",
+                        "VEDLEGG: Dette er din månedlige pensjon før skatt. Versjon 1	",
+                        "V00003",
+                        "00001"));
+        vedleggMap.put("AP_OPPL_BER_V1", () ->
+                new DoksysVedlegg(
+                        "AP_OPPL_BER_V1",
+                        "VEDLEGG: Opplysninger brukt i beregningen. Versjon 1",
+                        "V00004",
+                        "00001"));
+        vedleggMap.put("RETTIGH_V1", () ->
+                new DoksysVedlegg(
+                        "RETTIGH_V1",
+                        "VEDLEGG: Rettigheter. Versjon 1",
+                        "V00001",
+                        "00001"));
+        vedleggMap.put("AP_OPPL_BER_END_V1", () ->
+                new DoksysVedlegg(
+                        "AP_OPPL_BER_END_V1",
+                        "VEDLEGG: Opplysninger brukt i beregningen endret uttaksgrad. Versjon 1",
+                        "V00005",
+                        "00001"));
+        vedleggMap.put("AFP_PRIV_MND_UTB_V1", () ->
+                new DoksysVedlegg(
+                        "AFP_PRIV_MND_UTB_V1",
+                        "VEDLEGG: Dette er din månedlige pensjon før skatt. Versjon 1",
+                        "V00008",
+                        "00001"));
+        vedleggMap.put("AP_AVDOD_OPPL_BER_V1", () ->
+                new DoksysVedlegg(
+                        "AP_AVDOD_OPPL_BER_V1",
+                        "VEDLEGG: Opplysninger om avdøde brukt i beregningen.",
+                        "V00006",
+                        "00001"));
+        vedleggMap.put("AP_MND_UTB_V4", () ->
+                new DoksysVedlegg(
+                        "AP_MND_UTB_V4",
+                        "VEDLEGG: Dette er din månedlige pensjon før skatt. Versjon 4",
+                        "V00007",
+                        "00001"));
+
     }
 
     public List<DoksysVedlegg> map(String... vedleggCodes) {
