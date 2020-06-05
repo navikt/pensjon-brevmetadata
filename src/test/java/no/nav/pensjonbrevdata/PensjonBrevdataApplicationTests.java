4019af008c5e922aac3c0e8d2f884ddcf8b4510f
Ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2020-06-05 12:29:23 +0200

    Mock Unleash durit ctx test to avoid actual remote calls

--- src/test/java/no/nav/pensjonbrevdata/PensjonBrevdataApplicationTests.java
+++ src/test/java/no/nav/pensjonbrevdata/PensjonBrevdataApplicationTests.java
@@ -2,9 +2,15 @@ package no.nav.pensjonbrevdata;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import no.finn.unleash.Unleash;
 
 @SpringBootTest
 class PensjonBrevdataApplicationTests {
+
+    @MockBean
+    private Unleash defaultUnleash;
 
     @Test
     void contextLoads() {
