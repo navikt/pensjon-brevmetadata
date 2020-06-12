9fe710babbb59a671880477f7b69db8e06d84d37
Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
2020-06-12 13:21:02 +0200

    Refaktorerte komponent-test

--- src/test/java/no/nav/pensjonbrevdata/KomponentTest.java
+++ src/test/java/no/nav/pensjonbrevdata/KomponentTest.java
@@ -2,18 +2,17 @@ package no.nav.pensjonbrevdata;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
 import no.finn.unleash.Unleash;
 import no.nav.pensjonbrevdata.mappers.BrevdataMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -23,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
+import static com.google.gson.JsonParser.parseString;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 
@@ -37,35 +37,27 @@ public class KomponentTest {
 
     @Test
     public void testGetSprakForBrevkode() {
-        brevkoder().forEach((E) this::testGetSprakForBrevkode);
-    }
-    private void testGetSprakForBrevkode(String brevkode) throws IOException, InterruptedException, URISyntaxException {
-        HttpResponse<String> resp = HttpClient.newHttpClient().send(HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:"+port+"/api/brevdata/sprakForBrevkode/"+brevkode))
-                .build(), HttpResponse.BodyHandlers.ofString());
-        assertEquals("Feil i respons til brevkode "+brevkode,200, resp.statusCode());
-        assertEquals("Feil i respons til brevkode "+brevkode, loadResultSprakForBrevkode(brevkode), resp.body());
-    }
-
-    private String loadResultSprakForBrevkode(String brevkode) throws IOException, URISyntaxException {
-        return Files.readString(Path.of(getClass().getResource("/getSprakForBrevkode/"+brevkode).toURI()));
+        brevkoder().forEach((E) brevkode -> testEndpoint("sprakForBrevkode",brevkode));
     }
 
     @Test
     public void testGetBrevForBrevkode() {
-        brevkoder().forEach((E) this::testGetBrevForBrevkode);
+        brevkoder().forEach((E) brevkode -> testEndpoint("brevForBrevkode",brevkode));
     }
 
-    private void testGetBrevForBrevkode(String brevkode) throws IOException, InterruptedException, URISyntaxException {
+    private void testEndpoint(String endpoint, String brevkode) throws IOException, InterruptedException, URISyntaxException {
         HttpResponse<String> resp = HttpClient.newHttpClient().send(HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:"+port+"/api/brevdata/brevForBrevkode/"+brevkode))
+                .uri(URI.create("http://localhost:"+port+"/api/brevdata/"+endpoint+"/"+brevkode))
                 .build(), HttpResponse.BodyHandlers.ofString());
         assertEquals("Feil i respons til brevkode "+brevkode,200, resp.statusCode());
-        assertEquals("Feil i respons til brevkode "+brevkode, JsonParser.parseString(loadResultBrevForBrevkode(brevkode)), JsonParser.parseString(resp.body()));
+        assertEquals("Feil i respons til brevkode "+brevkode, parseString(loadResult(endpoint,brevkode)), parseString(resp.body()));
     }
 
-    private String loadResultBrevForBrevkode(String brevkode) throws IOException, URISyntaxException {
-        return Files.readString(Path.of(getClass().getResource("/getBrevForBrevkode/"+brevkode).toURI()));
+    private String loadResult(String endpoint, String brevkode) throws IOException, URISyntaxException {
+        URL url = getClass().getResource("/"+endpoint+"/" + brevkode);
+        if (url == null)
+            throw new IOException(endpoint+"-resultat ikke funnet for brevkode: " + brevkode);
+        return Files.readString(Path.of(url.toURI()));
     }
 
     private static List<String> brevkoder() {
@@ -97,11 +89,11 @@ public class KomponentTest {
         }
 
         private static void fixgetSprakForBrevkode(String brevkode) throws IOException {
-            Files.writeString(dir("getSprakForBrevkode").resolve(brevkode), toJSON(be.getSprakForBrevkode(brevkode)));
+            Files.writeString(dir("sprakForBrevkode").resolve(brevkode), toJSON(be.getSprakForBrevkode(brevkode)));
         }
 
         private static void fixgetBrevForBrevkode(String brevkode) throws IOException {
-            Files.writeString(dir("getBrevForBrevkode").resolve(brevkode), toJSON(be.getBrevForBrevkode(brevkode)));
+            Files.writeString(dir("brevForBrevkode").resolve(brevkode), toJSON(be.getBrevForBrevkode(brevkode)));
         }
 
         private static Path dir(String stringPath) throws IOException {
