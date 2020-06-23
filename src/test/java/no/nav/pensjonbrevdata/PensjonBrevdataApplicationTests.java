d5e95f499023afa1372c7751c5ec90c8fb7915f3
Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
2020-06-23 09:49:38 +0200

    La til test som sjekker at brevmal bare er med når includeXsd er true

--- src/test/java/no/nav/pensjonbrevdata/PensjonBrevdataApplicationTests.java
+++ src/test/java/no/nav/pensjonbrevdata/PensjonBrevdataApplicationTests.java
@@ -5,15 +5,42 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import no.finn.unleash.Unleash;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PensjonBrevdataApplicationTests {
 
     @MockBean
     private Unleash defaultUnleash;
 
+    @LocalServerPort
+    private int port;
+
     @Test
     void contextLoads() {
+    }
+
+
+    @Test
+    public void includeXsdShouldOnlyAffectPerRequest() throws IOException, InterruptedException {
+        var expected = getBrevdataForSaktypeResponse("KRIGSP", false).body();
+        getBrevdataForSaktypeResponse("KRIGSP", true).body();
+        var actual = getBrevdataForSaktypeResponse("KRIGSP", false).body();
+        assertEquals(expected, actual);
+    }
+
+    private HttpResponse<String> getBrevdataForSaktypeResponse(String brevkode, boolean includeXsd) throws IOException, InterruptedException {
+        return HttpClient.newHttpClient().send(HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:"+port+"/api/brevdata/brevdataForSaktype/" + brevkode + "?includeXsd="+includeXsd))
+                .build(), HttpResponse.BodyHandlers.ofString());
     }
 
 }
