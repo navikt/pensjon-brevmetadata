96cf100b2fdac6c8dda42d92fc054fad4a83988b
Håkon Heggholmen <hakon.heggholmen@nav.no>
2021-12-13 10:15:25 +0100

    Bumping vulnerable version of log4j.

--- src/main/java/no/nav/pensjonbrevdata/unleash/strategies/IsTest.java
+++ src/main/java/no/nav/pensjonbrevdata/unleash/strategies/IsTest.java
@@ -2,7 +2,6 @@ package no.nav.pensjonbrevdata.unleash.strategies;
 
 import java.util.Map;
 
-import net.logstash.logback.encoder.org.apache.commons.lang3.StringUtils;
 import no.finn.unleash.strategy.Strategy;
 
 public class IsTest implements Strategy {
