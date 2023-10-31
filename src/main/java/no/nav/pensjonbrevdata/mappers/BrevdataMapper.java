bbe764402d4c9d4f9e885d6e3cd0bb2f7cd704d7
Jeremy Elsom <jeremy.elsom@nav.no>
2023-10-31 13:09:06 +0100

    Updated 000148.xsd to v4.AlderspensjonPerManed and to v5.Alderspensjon

--- src/main/java/no/nav/pensjonbrevdata/mappers/BrevdataMapper.java
+++ src/main/java/no/nav/pensjonbrevdata/mappers/BrevdataMapper.java
@@ -117,17 +117,35 @@ public class BrevdataMapper {
             "00001",
             doksysVedleggMapper.map("RETTIGH_PLIKT_V1", "AP_MND_UTB_V1", "AP_OPPL_BER_END_V1"));
 
+    private static final Brevdata GAMMEL_AP_RETTING_AUTO =
+            new DoksysbrevV2("AP_RETTING_AUTO",
+                    false,
+                    "Vedtak - endring av alderspensjon pga rettelser",
+                    BrevkategoriCode.VEDTAK,
+                    DokumenttypeCode.U,
+                    Arrays.asList(SprakCode.NB, SprakCode.NN),
+                    true,
+                    BrevUtlandCode.ALLTID,
+                    null,
+                    BrevkravtypeCode.ALLE,
+                    DokumentkategoriCode.VB,
+                    true,
+                    BrevkontekstCode.VEDTAK,
+                    null,
+                    "000148",
+                    "00001",
+                    doksysVedleggMapper.map("AP_MND_UTB_V1", "RETTIGH_PLIKT_V1"));
+
+
     private static final Function<Map<String, Brevdata>, Map<String, Brevdata>> filtrerBrevMap =
             brevdataFiltrerBortNyttBrev(BRUK_VEDTAK_TILBAKEKREV, "VEDTAK_TILBAKEKREV")
                     .andThen(brevdataErstattMedGammeltBrev(PL_6567_ERSTATT_MED_GAMLE_BREV_OG_VEDLEGG, "AP_ENDR_GRAD_AUTO", GAMMEL_BREV_AP_ENDR_GRAD_AUTO))
                     .andThen(brevdataLeggTilGammeltBrev(PL_6567_ERSTATT_MED_GAMLE_BREV_OG_VEDLEGG, "PE_AP_04_227", GAMMEL_BREV_AP_ENDR_GRAD_AUTO))
-
                     .andThen(brevdataErstattMedGammeltBrev(PL_6567_ERSTATT_MED_GAMLE_BREV_OG_VEDLEGG, "AP_INNV_MAN", GAMMEL_AP_INNV_MAN))
+                    .andThen(brevdataErstattMedGammeltBrev(PL_6567_ERSTATT_MED_GAMLE_BREV_OG_VEDLEGG, "AP_RETTING_AUTO", GAMMEL_AP_RETTING_AUTO))
                     .andThen(brevdataErstattMedGammeltBrev(PL_6567_ERSTATT_MED_GAMLE_BREV_OG_VEDLEGG, "AP_ENDR_GRAD_MAN", GAMMEL_AP_ENDR_GRAD_MAN))
                     .andThen(brevdataErstattMedGammeltBrev(PL_6567_ERSTATT_MED_GAMLE_BREV_OG_VEDLEGG, "AP_ENDR_GJRETT_MAN", GAMMEL_AP_ENDR_GJRETT_MAN))
-
-                    .andThen(brevdataFiltrerBortNyttBrev(BRUK_AFP_INNV_MAN, "AFP_INNV_MAN"))
-            ;
+                    .andThen(brevdataFiltrerBortNyttBrev(BRUK_AFP_INNV_MAN, "AFP_INNV_MAN"));
 
     private final Map<String, Brevdata> brevMap;
 
