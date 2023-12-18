8d24a6ac62d718d6ef70adcc97edca2be8023c0c
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2023-12-18 12:04:28 +0100

    Legg til ny feature-toggle for vedlegg3

--- src/main/java/no/nav/pensjonbrevdata/config/BrevdataFeature.java
+++ src/main/java/no/nav/pensjonbrevdata/config/BrevdataFeature.java
@@ -9,6 +9,7 @@ public final class BrevdataFeature {
     public static final String BRUK_AFP_INNV_MAN = "pensjonsbrev.brevmetadata.afp-innv-man";
     public static final String ERSTATT_AFP_PRIV_MND_UTB_V1 = "pensjonsbrev.brevmetadata.afp-priv-mnd-utb-v1";
     public static final String PL_6567_ERSTATT_MED_GAMLE_BREV_OG_VEDLEGG = "pensjonsbrev.brevmetadata.pl-6567";
+    public static final String PL_6567_VEDLEGG3_GJT = "pensjonsbrev.brevmetadata.pl-6567-vedlegg3";
 
     public static final String PL_6972_ERSTATT_MED_GAMLE_BREV = "pensjonsbrev.brevmetadata.pl-6972";
 
