b45889f9e9710bdec30b4669c7b589d728ab1dfc
Jeremy Elsom <jeremy.elsom@nav.no>
2024-05-08 09:29:25 +0200

    Reverted the change in the toggle name: P_000069_v3AVDOD_ERSTATT_MED_GAMMELT_BREV = "pensjonsbrev.brevmetadata.p-000069-v3Avdod;

--- src/main/java/no/nav/pensjonbrevdata/config/BrevdataFeature.java
+++ src/main/java/no/nav/pensjonbrevdata/config/BrevdataFeature.java
@@ -4,7 +4,7 @@ public final class BrevdataFeature {
 
     public static final String P_000064_MED_AP2025_ERSTATT_MED_GAMMELT_BREV = "pensjonsbrev.brevmetadata.p-000064-med-ap2025";
     public static final String BRUK_AP_INNV_MAN_AP2025 = "pensjonsbrev.brevmetadata.bruk_ap_innv_man_ap2025";
-    public static final String P_000069_v3AVDOD_ERSTATT_MED_GAMMELT_BREV = "pensjonsbrev.brevmetadata.p-000069-v3Avdod";
+    public static final String P_000069_v3AVDOD_ERSTATT_MED_GAMMELT_BREV = "pensjonsbrev.brevmetadata.p-000069-v3Avdod;";
     public static final String BRUK_AFP_INNV_MAN = "pensjonsbrev.brevmetadata.afp-innv-man";
     public static final String ERSTATT_AFP_PRIV_MND_UTB_V1 = "pensjonsbrev.brevmetadata.afp-priv-mnd-utb-v1";
 
