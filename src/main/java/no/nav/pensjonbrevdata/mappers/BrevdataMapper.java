a9745bd95e63bb331fa932e06313bbc3400b9bb1
Jeremy Elsom <jeremy.elsom@nav.no>
2024-06-25 14:10:35 +0200

    Lagt versjonerte vedlegg til 000070

--- src/main/java/no/nav/pensjonbrevdata/mappers/BrevdataMapper.java
+++ src/main/java/no/nav/pensjonbrevdata/mappers/BrevdataMapper.java
@@ -4607,7 +4607,7 @@ public class BrevdataMapper {
                         null,
                         "000070",
                         "00001",
-                        null));
+                        doksysVedleggMapper.map("RETTIGH_PLIKT_V1", "AP_MND_UTB_V1")));
         brevMap.put("UT_DOD_ENSLIG_AUTO",
                 new Doksysbrev("UT_DOD_ENSLIG_AUTO",
                         false,
