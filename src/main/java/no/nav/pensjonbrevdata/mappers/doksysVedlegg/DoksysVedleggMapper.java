bf3a183369eec72f9bb2a81e906d208eaa3ef661
Mads Opheim <mads.opheim@nav.no>
2025-12-18 16:38:47 +0100

    Kan forenkle bort konstruktør-bruken her ved å sette opp map-et direkte

--- src/main/java/no/nav/pensjonbrevdata/mappers/doksysVedlegg/DoksysVedleggMapper.java
+++ src/main/java/no/nav/pensjonbrevdata/mappers/doksysVedlegg/DoksysVedleggMapper.java
@@ -3,85 +3,71 @@ package no.nav.pensjonbrevdata.mappers.doksysVedlegg;
 import no.nav.pensjonbrevdata.model.DoksysVedlegg;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
+import static java.util.Map.entry;
+
 public class DoksysVedleggMapper {
 
-    private final Map<String, DoksysVedlegg> vedleggMap;
-
-    public DoksysVedleggMapper() {
-        vedleggMap = new HashMap<>();
-        vedleggMap.put("AP2025_OPPL_BER_V1",
-                new DoksysVedlegg(
-                        "AP2025_OPPL_BER_V1",
-                        "VEDLEGG: Opplysninger brukt i beregningen. AP2025",
-                        "V00011",
-                        "00001"));
-        vedleggMap.put("RETTIGH_V1",
-                new DoksysVedlegg(
-                        "RETTIGH_V1",
-                        "VEDLEGG: Rettigheter. Versjon 1",
-                        "V00001",
-                        "00001"));
-        vedleggMap.put("RETTIGH_PLIKT_V1",
-                new DoksysVedlegg(
-                        "RETTIGH_PLIKT_V1",
-                        "VEDLEGG: Orientering om rettigheter og plikter. Versjon 1",
-                        "V00002",
-                        "00001"));
-        vedleggMap.put("AP_MND_UTB_V1",
-                new DoksysVedlegg(
-                        "AP_MND_UTB_V1",
-                        "VEDLEGG: Dette er din månedlige pensjon før skatt. Versjon 1	",
-                        "V00003",
-                        "00001"));
-        vedleggMap.put("AP_MND_UTB_AP2025",
-                new DoksysVedlegg(
-                        "AP_MND_UTB_AP2025",
-                        "VEDLEGG: Dette er din månedlige pensjon før skatt. Versjon 1	",
-                        "V00010",
-                        "00001"));
-        vedleggMap.put("AP_OPPL_BER_V1",
-                new DoksysVedlegg(
-                        "AP_OPPL_BER_V1",
-                        "VEDLEGG: Opplysninger brukt i beregningen. Versjon 1",
-                        "V00004",
-                        "00001"));
-        vedleggMap.put("AP_OPPL_BER_END_V1",
-                new DoksysVedlegg(
-                        "AP_OPPL_BER_END_V1",
-                        "VEDLEGG: Opplysninger brukt i beregningen endret uttaksgrad. Versjon 1",
-                        "V00005",
-                        "00001"));
-        vedleggMap.put("AP_AVDOD_OPPL_BER_V1",
-                new DoksysVedlegg(
-                        "AP_AVDOD_OPPL_BER_V1",
-                        "VEDLEGG: Opplysninger om avdøde brukt i beregningen.",
-                        "V00006",
-                        "00001"));
-        vedleggMap.put("AP_MND_UTB_V4",
-                new DoksysVedlegg(
-                        "AP_MND_UTB_V4",
-                        "VEDLEGG: Dette er din månedlige pensjon før skatt. Versjon 4",
-                        "V00007",
-                        "00001"));
-        vedleggMap.put("AFP_PRIV_MND_UTB_V1",
-                new DoksysVedlegg(
-                        "AFP_PRIV_MND_UTB_V1",
-                        "VEDLEGG: Dette er din månedlige pensjon før skatt. Versjon 1",
-                        "V00008",
-                        "00001"));
-        vedleggMap.put("INFO_MEDLEM_HELSE_V1",
-                new DoksysVedlegg(
-                        "INFO_MEDLEM_HELSE_V1",
-                        "VEDLEGG: Informasjon om medlemskap i folketrygden og rett til helsetjenester",
-                        "V00009",
-                        "00001"));
-
-    }
+    private final Map<String, DoksysVedlegg> vedleggMap = Map.ofEntries(
+            entry("AP2025_OPPL_BER_V1", new DoksysVedlegg(
+                    "AP2025_OPPL_BER_V1",
+                    "VEDLEGG: Opplysninger brukt i beregningen. AP2025",
+                    "V00011",
+                    "00001")),
+            entry("RETTIGH_V1", new DoksysVedlegg(
+                    "RETTIGH_V1",
+                    "VEDLEGG: Rettigheter. Versjon 1",
+                    "V00001",
+                    "00001")),
+            entry("RETTIGH_PLIKT_V1", new DoksysVedlegg(
+                    "RETTIGH_PLIKT_V1",
+                    "VEDLEGG: Orientering om rettigheter og plikter. Versjon 1",
+                    "V00002",
+                    "00001")),
+            entry("AP_MND_UTB_V1", new DoksysVedlegg(
+                    "AP_MND_UTB_V1",
+                    "VEDLEGG: Dette er din månedlige pensjon før skatt. Versjon 1	",
+                    "V00003",
+                    "00001")),
+            entry("AP_MND_UTB_AP2025", new DoksysVedlegg(
+                    "AP_MND_UTB_AP2025",
+                    "VEDLEGG: Dette er din månedlige pensjon før skatt. Versjon 1	",
+                    "V00010",
+                    "00001")),
+            entry("AP_OPPL_BER_V1", new DoksysVedlegg(
+                    "AP_OPPL_BER_V1",
+                    "VEDLEGG: Opplysninger brukt i beregningen. Versjon 1",
+                    "V00004",
+                    "00001")),
+            entry("AP_OPPL_BER_END_V1", new DoksysVedlegg(
+                    "AP_OPPL_BER_END_V1",
+                    "VEDLEGG: Opplysninger brukt i beregningen endret uttaksgrad. Versjon 1",
+                    "V00005",
+                    "00001")),
+            entry("AP_AVDOD_OPPL_BER_V1", new DoksysVedlegg(
+                    "AP_AVDOD_OPPL_BER_V1",
+                    "VEDLEGG: Opplysninger om avdøde brukt i beregningen.",
+                    "V00006",
+                    "00001")),
+            entry("AP_MND_UTB_V4", new DoksysVedlegg(
+                    "AP_MND_UTB_V4",
+                    "VEDLEGG: Dette er din månedlige pensjon før skatt. Versjon 4",
+                    "V00007",
+                    "00001")),
+            entry("AFP_PRIV_MND_UTB_V1", new DoksysVedlegg(
+                    "AFP_PRIV_MND_UTB_V1",
+                    "VEDLEGG: Dette er din månedlige pensjon før skatt. Versjon 1",
+                    "V00008",
+                    "00001")),
+            entry("INFO_MEDLEM_HELSE_V1", new DoksysVedlegg(
+                    "INFO_MEDLEM_HELSE_V1",
+                    "VEDLEGG: Informasjon om medlemskap i folketrygden og rett til helsetjenester",
+                    "V00009",
+                    "00001"))
+    );
 
     public Supplier<List<DoksysVedlegg>> map(String... vedleggCodes) {
         return () -> Arrays.stream(vedleggCodes).map(vedleggMap::get).toList();
