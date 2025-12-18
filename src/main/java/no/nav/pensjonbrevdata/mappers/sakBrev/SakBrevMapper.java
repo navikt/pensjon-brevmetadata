bf3251cb62352f88be89c44321405cce00b3ebb8
Mads Opheim <mads.opheim@nav.no>
2025-12-18 13:55:35 +0100

    Slettar map som no er ubrukt

--- src/main/java/no/nav/pensjonbrevdata/mappers/sakBrev/SakBrevMapper.java
+++ src/main/java/no/nav/pensjonbrevdata/mappers/sakBrev/SakBrevMapper.java
@@ -9,24 +9,13 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-import static no.nav.pensjonbrevdata.config.BrevdataFeature.BRUK_AFP_INNV_MAN;
-import static no.nav.pensjonbrevdata.unleash.UnleashProvider.toggle;
 
 @Service
 public class SakBrevMapper {
     private final SakBrevMap sakToBrevMap = new SakBrevMap();
     private final BrevdataMapper brevdataMapper = new BrevdataMapper();
-
-    // Brevkoder som skal legges til, men kun når feature er aktivert. På denne måten kan en kode legges til i dev og testes,
-    // og deretter legges til i produksjon. Man kan fjerne et innslag her når det er testet og fungerer i produksjon.
-    // Nøkkel skal være brevkode og verdi skal være feature-toggle.
-    public static final Map<String, UnleashProvider.Toggle> addedBrevkoder = Map.of(
-            "AFP_INNV_MAN", toggle(BRUK_AFP_INNV_MAN)
-    );
 
     public List<String> map(String saktype) {
         Set<String> koder = mapIgnoreFeatureToggle(saktype).stream().filter(brevKode -> !brevKode.equals("AFP_INNV_MAN")).collect(Collectors.toSet());
