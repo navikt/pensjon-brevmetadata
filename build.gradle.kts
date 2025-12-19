3f95efe1da9b04b4510ef5cc2013cc1f26c57fe1
Mads Opheim <mads.opheim@nav.no>
2025-12-19 13:46:32 +0100

    Gradle-oppsett

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+}
+
+val javaTarget: String by System.getProperties()
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.org.springframework.boot.spring.boot.starter.webmvc)
+    implementation(libs.io.prometheus.simpleclient.common)
+    implementation(libs.net.logstash.logback.logstash.logback.encoder)
+    implementation(libs.org.springdoc.springdoc.openapi.starter.webmvc.ui)
+    testImplementation(libs.org.springframework.boot.spring.boot.starter.webmvc.test)
+    testImplementation(libs.org.skyscreamer.jsonassert)
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.junit.platform.launcher)
+}
+
+group = "no.nav"
+version = "0.0.1-SNAPSHOT"
+description = "pensjon-brevdata"
+
+java {
+    targetCompatibility = JavaVersion.toVersion(javaTarget)
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
