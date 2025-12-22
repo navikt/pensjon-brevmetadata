ee06a6f93a65fb53c3fdedf94e33c60e2c07ff64
Mads Opheim <mads.opheim@nav.no>
2025-12-22 09:28:43 +0100

    Konverterer PrometheusFilter

--- src/main/java/no/nav/pensjonbrevdata/config/PrometheusFilter.kt
+++ src/main/java/no/nav/pensjonbrevdata/config/PrometheusFilter.kt
@@ -1,90 +1,90 @@
-package no.nav.pensjonbrevdata.config;
+package no.nav.pensjonbrevdata.config
 
-import io.prometheus.client.Counter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import io.prometheus.client.Counter
+import jakarta.servlet.*
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import java.io.IOException
+import java.util.Locale
 
 @Component
 @Order(10)
-public class PrometheusFilter implements Filter {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(PrometheusFilter.class);
-
-    static final Counter totalIncomingRequests = Counter.build().name("brevdata_rest_incoming_requests_total")
-            .help("Number of total requests")
-            .register();
-
-    static final Counter totalFailedRequests = Counter.build().name("brevdata_rest_failed_requests_total")
-            .help("Number of total failed requests (status >= 300)")
-            .register();
-
-    static final Counter totalFailedRequests300 = Counter.build().name("brevdata_rest_failed_requests_300_total")
-            .help("Number of total failed requests (status ~= 300)")
-            .register();
-
-    static final Counter totalFailedRequests400 = Counter.build().name("brevdata_rest_failed_requests_400_total")
-            .help("Number of total failed requests (status ~= 400)")
-            .register();
-
-    static final Counter totalFailedRequests500 = Counter.build().name("brevdata_rest_failed_requests_500_total")
-            .help("Number of total failed requests (status ~= 500)")
-            .register();
-
-    static final Counter totalSuccessfulRequests = Counter.build().name("brevdata_rest_successful_requests_total")
-            .help("Number of total failed requests (status == 2xx)")
-            .register();
-
-    static final List<String> skipUris = Arrays.asList("/api/internal/prometheus", "/api/internal/isalive", "/api/internal/isready", "/api/internal/docs", "/api/internal/docs/swagger-config");
-    static final String swaggerUriPattern = "/swagger-ui";
-
-    private boolean skipCounting(String uriPath) {
-        return skipUris.contains(uriPath.toLowerCase()) || uriPath.startsWith(swaggerUriPattern);
+class PrometheusFilter : Filter {
+    private fun skipCounting(uriPath: String): Boolean {
+        return skipUris.contains(uriPath.lowercase(Locale.getDefault())) || uriPath.startsWith(swaggerUriPattern)
     }
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
-
-        HttpServletRequest req = (HttpServletRequest) servletRequest;
+    @Throws(IOException::class, ServletException::class)
+    override fun doFilter(servletRequest: ServletRequest?, servletResponse: ServletResponse?, chain: FilterChain) {
+        val req = servletRequest as HttpServletRequest
         if (skipCounting(req.getServletPath())) {
-            chain.doFilter(servletRequest, servletResponse);
-            return;
+            chain.doFilter(servletRequest, servletResponse)
+            return
         }
 
-        totalIncomingRequests.inc();
+        totalIncomingRequests.inc()
 
-        LOGGER.info(req.getRequestURI());
+        LOGGER.info(req.getRequestURI())
 
         try {
-            chain.doFilter(servletRequest, servletResponse);
+            chain.doFilter(servletRequest, servletResponse)
         } finally {
-            HttpServletResponse response = (HttpServletResponse) servletResponse;
+            val response = servletResponse as HttpServletResponse
 
             if (response.getStatus() >= 300) {
-                LOGGER.error(req.getRequestURI() + " failing with code " + response.getStatus());
-                totalFailedRequests.inc();
+                LOGGER.error(req.getRequestURI() + " failing with code " + response.getStatus())
+                totalFailedRequests.inc()
                 if (response.getStatus() >= 500) {
-                    totalFailedRequests500.inc();
+                    totalFailedRequests500.inc()
                 } else if (response.getStatus() >= 400) {
-                    totalFailedRequests400.inc();
+                    totalFailedRequests400.inc()
                 } else if (response.getStatus() >= 300) {
-                    totalFailedRequests300.inc();
+                    totalFailedRequests300.inc()
                 }
             } else {
-                totalSuccessfulRequests.inc();
+                totalSuccessfulRequests.inc()
             }
         }
+    }
+
+    companion object {
+        private val LOGGER: Logger = LoggerFactory.getLogger(PrometheusFilter::class.java)
+
+        val totalIncomingRequests: Counter = Counter.build().name("brevdata_rest_incoming_requests_total")
+            .help("Number of total requests")
+            .register()
+
+        val totalFailedRequests: Counter = Counter.build().name("brevdata_rest_failed_requests_total")
+            .help("Number of total failed requests (status >= 300)")
+            .register()
+
+        val totalFailedRequests300: Counter = Counter.build().name("brevdata_rest_failed_requests_300_total")
+            .help("Number of total failed requests (status ~= 300)")
+            .register()
+
+        val totalFailedRequests400: Counter = Counter.build().name("brevdata_rest_failed_requests_400_total")
+            .help("Number of total failed requests (status ~= 400)")
+            .register()
+
+        val totalFailedRequests500: Counter = Counter.build().name("brevdata_rest_failed_requests_500_total")
+            .help("Number of total failed requests (status ~= 500)")
+            .register()
+
+        val totalSuccessfulRequests: Counter = Counter.build().name("brevdata_rest_successful_requests_total")
+            .help("Number of total failed requests (status == 2xx)")
+            .register()
+
+        val skipUris: MutableList<String?> = mutableListOf<String?>(
+            "/api/internal/prometheus",
+            "/api/internal/isalive",
+            "/api/internal/isready",
+            "/api/internal/docs",
+            "/api/internal/docs/swagger-config"
+        )
+        const val swaggerUriPattern: String = "/swagger-ui"
     }
 }
