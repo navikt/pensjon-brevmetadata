7495c5e95075526610afc2d364322b10003ad786
Mads Opheim <mads.opheim@nav.no>
2025-12-22 10:08:12 +0100

    Konverterer PensjonBrevdataApplicationTests

--- src/test/java/no/nav/pensjonbrevdata/PensjonBrevdataApplicationTests.kt
+++ src/test/java/no/nav/pensjonbrevdata/PensjonBrevdataApplicationTests.kt
@@ -1,90 +1,105 @@
-package no.nav.pensjonbrevdata;
+package no.nav.pensjonbrevdata
 
-import io.prometheus.client.exporter.common.TextFormat;
-import org.apache.commons.lang3.StringUtils;
-import org.json.JSONException;
-import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.JSONParser;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
-import static org.junit.jupiter.api.Assertions.*;
+import io.prometheus.client.exporter.common.TextFormat
+import org.apache.commons.lang3.StringUtils
+import org.json.JSONException
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.skyscreamer.jsonassert.JSONParser
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import java.io.IOException
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.util.Locale
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class PensjonBrevdataApplicationTests {
-
+internal class PensjonBrevdataApplicationTests {
     @LocalServerPort
-    private int port;
+    private val port = 0
 
     @Test
-    void contextLoads() {
-    }
-
-    @Test
-    public void callForAP_ENDR_OPPTJ_MAN() throws IOException, InterruptedException, JSONException {
-        HttpResponse<String> response = HttpClient.newHttpClient()
-                .send(
-                        HttpRequest.newBuilder()
-                                .uri(URI.create("http://localhost:"+port+"/api/brevdata/brevForBrevkode/AP_ENDR_OPPTJ_MAN"))
-                                .build(),
-                        HttpResponse.BodyHandlers.ofString()
-                );
-        JSONParser.parseJSON(response.body());
-        assertNotNull(response.body());
-        HttpStatus status = HttpStatus.resolve(response.statusCode());
-        assertNotNull(status);
-        assertTrue(status.is2xxSuccessful());
-
+    fun contextLoads() {
     }
 
     @Test
-    public void includeXsdShouldOnlyAffectPerRequest() throws IOException, InterruptedException {
-        var expected = getBrevdataForSaktypeResponse("KRIGSP", false).body();
-        getBrevdataForSaktypeResponse("KRIGSP", true).body();
-        var actual = getBrevdataForSaktypeResponse("KRIGSP", false).body();
-        assertEquals(expected, actual);
+    @Throws(IOException::class, InterruptedException::class, JSONException::class)
+    fun callForAP_ENDR_OPPTJ_MAN() {
+        val response = HttpClient.newHttpClient()
+            .send<String?>(
+                HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:" + port + "/api/brevdata/brevForBrevkode/AP_ENDR_OPPTJ_MAN"))
+                    .build(),
+                HttpResponse.BodyHandlers.ofString()
+            )
+        JSONParser.parseJSON(response.body())
+        Assertions.assertNotNull(response.body())
+        val status = HttpStatus.resolve(response.statusCode())
+        Assertions.assertNotNull(status)
+        Assertions.assertTrue(status!!.is2xxSuccessful())
     }
 
     @Test
-    public void shouldReturnAsPlainTextForPrometheus() throws IOException, InterruptedException {
-        HttpResponse response = HttpClient.newHttpClient()
-                .send(
-                        HttpRequest.newBuilder()
-                                .uri(URI.create("http://localhost:"+port+"/api/internal/prometheus"))
-                                .build(),
-                        HttpResponse.BodyHandlers.ofString()
-                );
-
-        assertEquals(HttpStatus.OK.value(), response.statusCode());
-        assertEquals(StringUtils.deleteWhitespace(TextFormat.CONTENT_TYPE_004), StringUtils.deleteWhitespace(response.headers().firstValue("Content-Type").get().toLowerCase()));
+    @Throws(IOException::class, InterruptedException::class)
+    fun includeXsdShouldOnlyAffectPerRequest() {
+        val expected = getBrevdataForSaktypeResponse("KRIGSP", false).body()
+        getBrevdataForSaktypeResponse("KRIGSP", true).body()
+        val actual = getBrevdataForSaktypeResponse("KRIGSP", false).body()
+        Assertions.assertEquals(expected, actual)
     }
 
     @Test
-    public void shouldMatchTrailingSlash() throws IOException, InterruptedException {
-        HttpResponse response = HttpClient.newHttpClient()
-                .send(
-                        HttpRequest.newBuilder()
-                                .uri(URI.create("http://localhost:"+port+"/api/brevdata/allBrev/"))
-                                .build(),
-                        HttpResponse.BodyHandlers.ofString()
-                );
+    @Throws(IOException::class, InterruptedException::class)
+    fun shouldReturnAsPlainTextForPrometheus() {
+        val response: HttpResponse<*> = HttpClient.newHttpClient()
+            .send<String?>(
+                HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:" + port + "/api/internal/prometheus"))
+                    .build(),
+                HttpResponse.BodyHandlers.ofString()
+            )
 
-        assertEquals(HttpStatus.OK.value(), response.statusCode());
-        assertEquals(MediaType.APPLICATION_JSON_VALUE, StringUtils.deleteWhitespace(response.headers().firstValue("Content-Type").get().toLowerCase()));
+        Assertions.assertEquals(HttpStatus.OK.value(), response.statusCode())
+        Assertions.assertEquals(
+            StringUtils.deleteWhitespace(TextFormat.CONTENT_TYPE_004), StringUtils.deleteWhitespace(
+                response.headers().firstValue("Content-Type").get().lowercase(
+                    Locale.getDefault()
+                )
+            )
+        )
     }
 
-    private HttpResponse<String> getBrevdataForSaktypeResponse(String brevkode, boolean includeXsd) throws IOException, InterruptedException {
-        return HttpClient.newHttpClient().send(HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:"+port+"/api/brevdata/brevdataForSaktype/" + brevkode + "?includeXsd="+includeXsd))
-                .build(), HttpResponse.BodyHandlers.ofString());
+    @Test
+    @Throws(IOException::class, InterruptedException::class)
+    fun shouldMatchTrailingSlash() {
+        val response: HttpResponse<*> = HttpClient.newHttpClient()
+            .send<String?>(
+                HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:" + port + "/api/brevdata/allBrev/"))
+                    .build(),
+                HttpResponse.BodyHandlers.ofString()
+            )
+
+        Assertions.assertEquals(HttpStatus.OK.value(), response.statusCode())
+        Assertions.assertEquals(
+            MediaType.APPLICATION_JSON_VALUE, StringUtils.deleteWhitespace(
+                response.headers().firstValue("Content-Type").get().lowercase(
+                    Locale.getDefault()
+                )
+            )
+        )
     }
 
+    @Throws(IOException::class, InterruptedException::class)
+    private fun getBrevdataForSaktypeResponse(brevkode: String?, includeXsd: Boolean): HttpResponse<String?> {
+        return HttpClient.newHttpClient().send<String?>(
+            HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:" + port + "/api/brevdata/brevdataForSaktype/" + brevkode + "?includeXsd=" + includeXsd))
+                .build(), HttpResponse.BodyHandlers.ofString()
+        )
+    }
 }
