45130e8a97295fe22bb5a56bb6008662326e90fc
Mads Opheim <mads.opheim@nav.no>
2025-12-22 09:34:16 +0100

    Konverterer DoksysVedlegg

--- src/main/java/no/nav/pensjonbrevdata/model/DoksysVedlegg.kt
+++ src/main/java/no/nav/pensjonbrevdata/model/DoksysVedlegg.kt
@@ -1,57 +1,35 @@
-package no.nav.pensjonbrevdata.model;
+package no.nav.pensjonbrevdata.model
 
-import kotlin.jvm.functions.Function1;
+import kotlin.Function1
 
-import java.util.function.Function;
+class DoksysVedlegg protected constructor(
+    val vedleggkode: String?,
+    val dekode: String?,
+    val dokumentmalId: String?,
+    val dokumentmalFelleselementId: String?,
+    val dokumentmal: String?,
+    val dokumentmalFelleselement: String?
+) {
+    constructor(
+        vedleggkode: String?,
+        dekode: String?,
+        dokumentmalId: String?,
+        dokumentmalFelleselementId: String?
+    ) : this(vedleggkode, dekode, dokumentmalId, dokumentmalFelleselementId, null, null)
 
-public class DoksysVedlegg {
-    protected final String vedleggkode;
-    protected final String dekode;
-    protected final String dokumentmalId;
-    protected final String dokumentmalFelleselementId;
-    protected final String dokumentmal;
-    protected final String dokumentmalFelleselement;
-
-    public DoksysVedlegg(String vedleggkode, String dekode, String dokumentmalId, String dokumentmalFelleselementId) {
-        this(vedleggkode, dekode, dokumentmalId, dokumentmalFelleselementId, null, null);
-    }
-
-    protected DoksysVedlegg(String vedleggkode, String dekode, String dokumentmalId, String dokumentmalFelleselementId, String dokumentmal, String dokumentmalFelleselement) {
-        this.vedleggkode = vedleggkode;
-        this.dekode = dekode;
-        this.dokumentmalId = dokumentmalId;
-        this.dokumentmalFelleselementId = dokumentmalFelleselementId;
-        this.dokumentmal = dokumentmal;
-        this.dokumentmalFelleselement = dokumentmalFelleselement;
-    }
-
-    public String getDekode() {
-        return dekode;
-    }
-
-    public String getDokumentmalId() {
-        return dokumentmalId;
-    }
-
-    public String getDokumentmalFelleselementId() {
-        return dokumentmalFelleselementId;
-    }
-
-    public String getDokumentmal() {
-        return dokumentmal;
-    }
-
-    public String getDokumentmalFelleselement() {
-        return dokumentmalFelleselement;
-    }
-
-    public String getVedleggkode() {
-        return vedleggkode;
-    }
-
-    public DoksysVedlegg medXSD(Function1<String, String> dokumentmalGenerator, Function1<String, String> fellesmalGenerator) {
-        String dokumentmal = dokumentmalGenerator.invoke(dokumentmalId);
-        String dokumentmalFelleselement = fellesmalGenerator.invoke(dokumentmalFelleselementId);
-        return new DoksysVedlegg(vedleggkode, dekode, dokumentmalId, dokumentmalFelleselementId, dokumentmal, dokumentmalFelleselement);
+    fun medXSD(
+        dokumentmalGenerator: Function1<String?, String?>,
+        fellesmalGenerator: Function1<String?, String?>
+    ): DoksysVedlegg {
+        val dokumentmal = dokumentmalGenerator.invoke(dokumentmalId)
+        val dokumentmalFelleselement = fellesmalGenerator.invoke(dokumentmalFelleselementId)
+        return DoksysVedlegg(
+            vedleggkode,
+            dekode,
+            dokumentmalId,
+            dokumentmalFelleselementId,
+            dokumentmal,
+            dokumentmalFelleselement
+        )
     }
 }
