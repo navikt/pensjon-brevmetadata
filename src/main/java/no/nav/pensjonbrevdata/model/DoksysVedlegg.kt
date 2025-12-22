9be0b34be967ca4bce6ae3b4aff161425bc9b7f6
Mads Opheim <mads.opheim@nav.no>
2025-12-22 11:34:40 +0100

    Forenklar

--- src/main/java/no/nav/pensjonbrevdata/model/DoksysVedlegg.kt
+++ src/main/java/no/nav/pensjonbrevdata/model/DoksysVedlegg.kt
@@ -6,28 +6,21 @@ class DoksysVedlegg private constructor(
     val dokumentmalId: String,
     val dokumentmalFelleselementId: String,
     val dokumentmal: String?,
-    val dokumentmalFelleselement: String?
+    val dokumentmalFelleselement: String?,
 ) {
     constructor(
         vedleggkode: String?,
         dekode: String?,
         dokumentmalId: String,
-        dokumentmalFelleselementId: String
+        dokumentmalFelleselementId: String,
     ) : this(vedleggkode, dekode, dokumentmalId, dokumentmalFelleselementId, null, null)
 
-    fun medXSD(
-        dokumentmalGenerator: (String) -> String,
-        fellesmalGenerator: (String) -> String
-    ): DoksysVedlegg {
-        val dokumentmal = dokumentmalGenerator.invoke(dokumentmalId)
-        val dokumentmalFelleselement = fellesmalGenerator.invoke(dokumentmalFelleselementId)
-        return DoksysVedlegg(
-            vedleggkode,
-            dekode,
-            dokumentmalId,
-            dokumentmalFelleselementId,
-            dokumentmal,
-            dokumentmalFelleselement
-        )
-    }
+    fun medXSD(dokumentmalGenerator: (String) -> String, fellesmalGenerator: (String) -> String) = DoksysVedlegg(
+        vedleggkode,
+        dekode,
+        dokumentmalId,
+        dokumentmalFelleselementId,
+        dokumentmalGenerator.invoke(dokumentmalId),
+        fellesmalGenerator.invoke(dokumentmalFelleselementId)
+    )
 }
