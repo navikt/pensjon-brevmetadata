ffb00d05283740229e5ecbfc10843d0174e7f36d
Mads Opheim <mads.opheim@nav.no>
2025-12-22 11:49:05 +0100

    Standardiserer toml-opplegg

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     java
     kotlin("jvm") version libs.versions.kotlinVersion
-    id("org.springframework.boot") version "4.0.1"
-    id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.spring") version "2.3.0"
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    kotlin("plugin.spring") version libs.versions.kotlinVersion
 }
 
 val javaTarget: String by System.getProperties()
@@ -20,7 +20,7 @@ dependencies {
     testImplementation(libs.org.springframework.boot.spring.boot.starter.webmvc.test)
     testImplementation(libs.org.skyscreamer.jsonassert)
     testImplementation(libs.junit.jupiter)
-    testImplementation("io.mockk:mockk:1.14.7")
+    testImplementation(libs.mockk)
     testRuntimeOnly(libs.junit.platform.launcher)
     implementation(kotlin("stdlib"))
 }
