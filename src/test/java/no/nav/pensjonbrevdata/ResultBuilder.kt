6d2214518aba475c68c5d0ef0a542d99385ce306
Mads Opheim <mads.opheim@nav.no>
2025-12-22 11:57:00 +0100

    Trekkjer ut det å byggje opp testdata til ei eiga fil, for oversikt og lettare køyring

---
+++ src/test/java/no/nav/pensjonbrevdata/ResultBuilder.kt
@@ -0,0 +1,62 @@
+package no.nav.pensjonbrevdata
+
+import com.fasterxml.jackson.core.util.DefaultIndenter
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.ObjectWriter
+import com.fasterxml.jackson.databind.json.JsonMapper
+import no.nav.pensjonbrevdata.TestDataHolder.brevkoder
+import no.nav.pensjonbrevdata.TestDataHolder.brevkoderIBrevSystem
+import no.nav.pensjonbrevdata.TestDataHolder.sakstyper
+import no.nav.pensjonbrevdata.mappers.brevdata.BrevdataMapper
+import no.nav.pensjonbrevdata.mappers.sakBrev.SakBrevMapper
+import java.nio.file.Files
+import java.nio.file.Path
+
+/**
+ * Bygger en base-line av responser som applikasjonen gjør akkurat nå, og som benyttes av KomponentTest
+ */
+object ResultBuilder {
+    private val be = BrevdataEndpoint(BrevdataProvider(BrevdataMapper(), SakBrevMapper()))
+    private val objectMapper: ObjectWriter = JsonMapper.builder()
+        .configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true)
+        .build()
+        .writer(
+            DefaultPrettyPrinter()
+                .withObjectIndenter(DefaultIndenter().withLinefeed("\n"))
+        )
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        for (brevkode in brevkoder()) {
+            writeString(brevkode, "brevForBrevkode", be.getBrevForBrevkode(brevkode))
+            writeString(brevkode, "sprakForBrevkode", be.getSprakForBrevkode(brevkode))
+        }
+        for (sakstype in sakstyper()) {
+            writeString(sakstype, "brevdataForSaktype/" + true, be.getBrevdataForSaktype(sakstype, true))
+            writeString(sakstype, "brevdataForSaktype/" + false, be.getBrevdataForSaktype(sakstype, false))
+            writeString(sakstype, "brevkoderForSaktype", be.getBrevkoderForSaktype(sakstype))
+        }
+        for (brevkodeIBrevsystem in brevkoderIBrevSystem()) {
+            writeString(
+                brevkodeIBrevsystem,
+                "brevKeyForBrevkodeIBrevsystem",
+                be.getBrevKeyForBrevkodeIBrevsystem(brevkodeIBrevsystem)
+            )
+        }
+
+        writeString("false", "allBrev", be.getAllBrev(false))
+        writeString("true", "allBrev", be.getAllBrev(true))
+    }
+
+    fun writeString(brevkode: String, dir: String?, toJSON: Any?) {
+        val path = Path.of("src", "test", "resources", dir)
+        Files.createDirectories(path)
+        Files.writeString(path.resolve(brevkode), toJSON(toJSON))
+    }
+
+    fun toJSON(`object`: Any?): CharSequence? {
+        if (`object` == null) return ""
+        return objectMapper.writeValueAsString(`object`)
+    }
+}
