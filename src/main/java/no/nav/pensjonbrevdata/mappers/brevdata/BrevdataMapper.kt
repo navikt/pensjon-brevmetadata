c045c26a3f2921580433f0d214c154aa47e53d22
Mads Opheim <mads.opheim@nav.no>
2025-12-22 10:57:46 +0100

    Forenklar

--- src/main/java/no/nav/pensjonbrevdata/mappers/brevdata/BrevdataMapper.kt
+++ src/main/java/no/nav/pensjonbrevdata/mappers/brevdata/BrevdataMapper.kt
@@ -1,31 +1,16 @@
 package no.nav.pensjonbrevdata.mappers.brevdata
 
 import no.nav.pensjonbrevdata.model.Brevdata
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class BrevdataMapper {
     private val brevMap = BrevdataMap()
-    private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
 
-    fun map(brevkode: String): Brevdata = brevMap.get().filter { it.key != "AFP_INNV_MAN" }[brevkode] ?: throw IllegalArgumentException("Brevkode \"$brevkode\" does not exist")
+    fun map(brevkode: String): Brevdata = brevMap.get().filter { it.key != "AFP_INNV_MAN" }[brevkode] ?: throw IllegalArgumentException("Brevkode $brevkode does not exist")
 
-    val allBrevAsList: MutableList<Brevdata>
-        get() {
-            val brevdataList: MutableList<Brevdata> = ArrayList()
-            val filtrertBrevMap: Map<String, Brevdata> = brevMap.get().filter { it.key != "AFP_INNV_MAN" }
-
-            for (brevdataCallable in filtrertBrevMap.values) {
-                try {
-                    brevdataList.add(brevdataCallable)
-                } catch (e: IllegalArgumentException) {
-                    logger.info("Illegal argument i getAllBrevAsList", e)
-                }
-            }
-            return brevdataList
-        }
+    val allBrevAsList: List<Brevdata>
+        get() = brevMap.get().filter { it.key != "AFP_INNV_MAN" }.map { it.value }
 
     fun getBrevKeysForBrevkodeIBrevsystem(brevkodeIBrevsystem: String?): List<String> = brevMap.get()
         .filter { it.key != "AFP_INNV_MAN" }
